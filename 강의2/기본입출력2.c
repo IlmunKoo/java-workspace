06f30b3eceba037d81d26a2d01692c4610c6ed70
IlmunKoo <59404684+IlmunKoo@users.noreply.github.com>
2020-03-02 17:41:57 +0900

    Add files via upload

---
+++ 강의2/기본입출력2.c
@@ -0,0 +1,17 @@
+/*#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main(void) {
+	double a; //실수형 입력 받음
+	scanf("%lf", &a);
+	//lf: double을 인풋으로 넣어 주겠다는 뜻
+	//&a: a라는 변수에 사용자가 값을 넣겠다는 뜻
+	printf("%.2f/n", a);
+	//백슬래시n: 행을 바꿈
+		//.2f를 쳤을 때: 소수점 이하 둘째자리까지 출력
+	//(%뒤에 점이 나오는 건 소수점 표현하기 위해)
+	//double은 받을 때 f로014 받음, 2는 소수점 이하 둘째자리
+	//소수점 이하 셋째자리에서 반올림
+	system("pause");
+	return 0;
+}*/
